c81199ceef79dfbb3c18becf5120ed4b6bf3641f
QimingZheng <zqmmqz@sjtu.edu.cn>
2019-01-12 21:18:09 +0800

    update

--- tools/batch.cpp
+++ tools/batch.cpp
@@ -3,69 +3,61 @@
 #include <string>
 #include <vector>
 #include "string.h"
+#include <boost/program_options.hpp>
 
 #include "ITA_regex.h"
 
+namespace bpo = boost::program_options;  
 using namespace std;
-
-void usage(char *program)
-{
-        cerr << "usage: " << program << " nfa_file input_string string_count [-s] [-p] [-k]" << endl;
-        cerr << "               -s: show regex matching result" << endl;
-        cerr << "               -p: show profiling results" << endl;
-        cerr << "               -k: kernel type [iNFA], [TKO], [AS], iNFA by default" << endl;
-}
 
 int main(int argc, char **argv)
 {
         bool show_match_result = false, profiler_mode = false;
+        string nfa_file, input_string, kernel_type;
+        int string_count;
         ITA_FLAGS flag=0;
+        
+        bpo::options_description opt("all options");
 
-        if (argc < 4 || argc > 8)
-        {
-                usage(argv[0]);
+        opt.add_options()
+        ("nfa,f", bpo::value<string>(&nfa_file)->default_value(""), "nfa_file")
+        ("input,i", bpo::value<string>(&input_string)->default_value(""), "input_string")
+        ("num,n", bpo::value<int>(&string_count)->default_value(0), "string_count")
+        ("s", "show regex matching result")
+        ("p", "show profiling result")
+        ("k", bpo::value<string>(&kernel_type)->default_value("iNFA"), "kernel type [iNFA], [TKO], [AS], iNFA by default")
+        ("help,h", "Helper");
+
+
+        bpo::variables_map vm;
+
+        try{
+                bpo::store(parse_command_line(argc, argv, opt), vm);  
+        }
+        catch(...){  
+                std::cout << "Undefined Arguments\n";  
                 return EXIT_FAILURE;
         }
 
-        if (argc >= 5)
-        {
-                if (strcmp(argv[4], "-s") && strcmp(argv[4], "-p"))
-                        return EXIT_FAILURE;
-                if (!strcmp(argv[4], "-s"))
-                        show_match_result = 1;
-                if (!strcmp(argv[4], "-p"))
-                        profiler_mode = 1;
+        bpo::notify(vm);
+
+        if(vm.count("help") ){  
+                cout << opt << std::endl;  
+                return EXIT_SUCCESS;
         }
 
-        if (argc >= 6)
-        {
-                if (strcmp(argv[5], "-s") && strcmp(argv[5], "-p"))
-                        return EXIT_FAILURE;
-                if (!strcmp(argv[5], "-s"))
-                        show_match_result = 1;
-                if (!strcmp(argv[5], "-p"))
-                        profiler_mode = 1;
-        }
+        if(vm.count("s")) show_results = 1;
+        if(vm.count("p")) profiler_mode = 1;
 
         flag |= (profiler_mode ? PROFILER_MODE : 0) | (show_match_result ? SHOW_RESULTS : 0);
 
-        if (argc >= 7)
-        {
-                if (argc != 8)
-                        exit(-1);
-                if (!strcmp(argv[7], "iNFA"))
-                        flag |= INFA_KERNEL;
-                if (!strcmp(argv[7], "TKO"))
-                        flag |= TKO_KERNEL;
-                if (!strcmp(argv[7], "AS"))
-                        flag |= AS_KERNEL;
-        }
-        else
-        {
+        if (!strcmp(kernel_type.c_str(), "iNFA"))
                 flag |= INFA_KERNEL;
-        }
+        if (!strcmp(kernel_type.c_str(), "TKO"))
+                flag |= TKO_KERNEL;
+        if (!strcmp(kernel_type.c_str(), "AS"))
+                flag |= AS_KERNEL;
 
-        int string_count = atoi(argv[3]);
         if (string_count <= 0)
         {
                 cerr << "Error: invalid string_count value " << string_count << endl;
@@ -76,16 +68,19 @@ int main(int argc, char **argv)
         char *h_input_array[string_count];   // array of string
         int input_bytes_array[string_count]; // array of string length
 
+        char* str = strdup(input_string.c_str());
+        char* nfa = strdup(nfa_file.c_str());
+
         for (int i = 0; i < string_count; i++)
         {
-                h_input_array[i] = argv[2];
-                input_bytes_array[i] = strlen(argv[2]);
+                h_input_array[i] = str;
+                input_bytes_array[i] = strlen(str);
         }
 
         vector<int> accepted_rules[string_count];
 
         BatchedScan(flag,
-                    argv[1],
+                    nfa,
                     h_input_array,
                     input_bytes_array,
                     string_count,
