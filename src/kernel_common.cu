759876fe3ac8f5b12d83175df0bba3a5e688a902
QimingZheng <zqmmqz@sjtu.edu.cn>
2019-01-12 10:48:11 +0800

    update

--- src/kernel_common.cu
+++ src/kernel_common.cu
@@ -1,4 +1,5 @@
 #include "kernel_common.h"
+#include <iomanip>
 
 __device__ bool is_word_char(unsigned char c)
 {       
@@ -7,17 +8,17 @@ __device__ bool is_word_char(unsigned char c)
 
 void show_results(int array_size, vector<ST_T> *final_states, vector<int> *accept_rules){
     for (int i = 0; i < array_size; i++) {
-        cout << "String " << i + 1 << endl;
+        cout << "String [" << i + 1 <<"]\n";
         
         cout << "Final States : ";
         for (int j = 0; j < final_states[i].size(); j++) {
-                cout << final_states[i][j] << " ";
+                cout << final_states[i][j] << ", ";
         }
         cout << endl;
 
         cout << "Accept Rules : ";
         for (int j = 0; j < accept_rules[i].size(); j++) {
-                cout << accept_rules[i][j] << " ";
+                cout << accept_rules[i][j] << ", ";
         }                        
         cout << endl;
     }
@@ -41,29 +42,29 @@ void Profiler(struct timeval start_time,
     float time = (end_time.tv_sec - start_time.tv_sec) * 1000.0 + (end_time.tv_usec - start_time.tv_usec) / 1000.0;
     float cuda_time = 0;
 
-    cout << "Total time: " << time << " ms ";
-    cout << "(Per string time: " << time / array_size << " ms)" << endl;
+    cout << setw(20) << "Total: " << setw(10) << time << " ms\n";
+    cout << setw(20) << "Per-string: " << setw(10) << time / array_size << " ms\n";
 
     cudaEventElapsedTime(&time, memalloc_start, memalloc_end);
-    cout << "Device memory allocation time: " << time << " ms" << endl;
+    cout << setw(20) << "Device MemAlloc: " << setw(10) << time << " ms\n";
     cuda_time += time;
 
     cudaEventElapsedTime(&time, memcpy_h2d_start, memcpy_h2d_end);
-    cout << "Memory copy host to device time: " << time << " ms" << endl;
+    cout << setw(20) << "MemCpy H2D: " << setw(10) << time << " ms\n";
     cuda_time += time;
 
     cudaEventElapsedTime(&time, kernel_start, kernel_end);
-    cout << "Kernel execution time: " << time << " ms ";
-    cout << "(Per string kernel execution time: " << time / array_size << " ms)" << endl;
+    cout << setw(20) << "Kernel Exec: " << setw(10) << time << " ms\n";
+    cout << setw(20) << "Per-string Kernel: " << setw(10) << time / array_size << " ms\n";
     cuda_time += time;
 
     cudaEventElapsedTime(&time, memcpy_d2h_start, memcpy_d2h_end);
-    cout << "Memory copy device to host time: " << time << " ms" << endl;
+    cout << setw(20) << "MemCpy D2H: " << setw(10) << time << " ms\n";
     cuda_time += time;
 
     cudaEventElapsedTime(&time, memfree_start, memfree_end);
-    cout << "Device memory free time: " << time << " ms" << endl;
+    cout << setw(20) << "Device MemFree: " << setw(10) << time << " ms\n";
     cuda_time += time;
 
-    cout << "CUDA related time: " << cuda_time << " ms" << endl;
+    cout << setw(20) << "CUDA time: " << setw(10) << cuda_time << " ms\n";
 }
